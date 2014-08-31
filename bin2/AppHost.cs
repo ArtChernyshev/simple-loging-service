a3f7dab5109fafa0bc055fe441dcca14e67b8439
cai <cai@2gis.ru>
2014-08-31 18:44:39 +0700

    we

--- bin2/AppHost.cs
+++ bin2/AppHost.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
+using DataAccess;
 using Funq;
 using ServiceStack;
 using ServiceStack.Mvc;
@@ -21,7 +23,13 @@ namespace MvcEmptyWebApp1
 		public AppHost()
 			: base("MvcEmptyWebApp1", typeof(MyServices).Assembly)
 		{
-
+			// System.Data.SQLite.dll сама может определить нативную сборку какой разрядности ей подгрузить для использования
+			// из папки x64 или x86. Но так как ASP.NET-сайты при выполнении грузят каждую сборку в отдельную временную папку, то 
+			// рядом с нашей сборкой DataAccess.dll не оказывается нативных SQLite сборок. Для разрешения таких ситуаций можно установить
+			// переменную среды PreLoadSQLite_BaseDirectory и указать где нужно искать нативные сборки.
+			// http://system.data.sqlite.org/index.html/artifact/0c51bf87d4a40dc4ea37d1fbee71daa13d9788df
+			string path = Assembly.GetAssembly(typeof(SqliteTileStorage)).CodeBase.Substring(8); // путь до изначального расположения сборок сервиса без префикса file:////
+			Environment.SetEnvironmentVariable("PreLoadSQLite_BaseDirectory", Path.GetDirectoryName(path));
 		}
 
 		/// <summary>
